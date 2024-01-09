dc0aa5e9b4026fa6cc48838214be61d18bfa71e5
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-01-09 09:37:33 -0300

    completed prep1 and prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        string letter = String.Empty;
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (gradePercentage <= 93 && gradePercentage >= 60)
+        {
+            if (gradePercentage % 10 >= 7)
+            {
+                letter = letter + "+";
+            }
+            else if (gradePercentage % 10 <= 3)
+            {
+                letter = letter + "-";
+            }
+        }
+        Console.WriteLine("Your letter grade is: " + letter);
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry. You failed the class. But I'm sure you'll pass next time!");
+        }
     }
 }
