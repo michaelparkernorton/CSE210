7739f91b3c1989b494e315a31ad4f850f1dc6e81
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-02-06 14:39:17 -0300

    clean up

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -10,21 +10,13 @@ class Program
         string scriptureText = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";
         Reference reference = new Reference("Proverbs", 3, 5, 6);
         Scripture scripture = new Scripture(reference, scriptureText);
-        Console.Clear();
-        scripture.DisplayScripture();
-        Console.WriteLine("Press enter to continue or type 'quit' to finish:");
         string quit = "";
-        quit = Console.ReadLine();
         bool running = true;
-        if (quit == "quit")
-        {
-            running = false;
-        }
         while (running) {
             Console.Clear();
             scripture.HideWords();
             scripture.DisplayScripture();
-            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            Console.WriteLine("\nPress enter to continue or type 'quit' to finish:");
             quit = Console.ReadLine();
             if (scripture.IsHidden() || quit == "quit")
             {
